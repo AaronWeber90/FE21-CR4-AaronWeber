b5eace13d3d47d0c64e31d60fbfe2a8e058651b9
AaronWeber90 <aaronweber@gmx.at>
2021-07-05 08:26:07 +0200

    add sort, buttons not working after sort

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
+// CREATE CONTENT FOR MOVIE CONTAINER
+let movieArr = JSON.parse(movies);
 
-let movieArr = JSON.parse(movies)
-
-console.log(movieArr)
-
-let result = ""
+let result = "";
 
 for (let i = 0; i < movieArr.length; i++) {
   result += `
@@ -19,12 +17,36 @@ for (let i = 0; i < movieArr.length; i++) {
   </div>`
 }
 
-
-
 document.getElementById("movie-container").innerHTML = result;
 
 
 
+// CREATE SORTED ARRAY
+document.getElementById("sort-btn").addEventListener ("click", movieSort);
+
+function movieSort() {
+  movieArr = movieArr.sort((a, b) => a.likes - b.likes);
+  let descArr = "";
+  console.log(movieArr)
+  for (let i = 0; i < movieArr.length; i++) {
+    descArr += `
+    <div class="media p-1 flex-grow-1">
+    <img src="${movieArr[i].image}" alt="" class="border">
+    <div class="media-body p-3 d-flex flex-column justify-content-between">
+      <div class="text-left">
+          <h4>${movieArr[i].movieName}</h4>
+          <p>${movieArr[i].description}</p>
+      </div>  
+      <div class="text-right"><span id="like-btn" class="like-link">Like <i class="fas fa-thumbs-up"></i></span><span class="like-number rounded-circle">${movieArr[i].likes}</span></div>
+    </div>
+    </div>`
+  };
+  document.getElementById("movie-container").innerHTML = descArr;
+}
+
+
+
+// CREATE LIKE BUTTON - not workin after sporting
 let likeLink = document.getElementsByClassName("like-link");
 
 for (let i = 0; i < likeLink.length; i++) {
@@ -37,13 +59,3 @@ function likeIncrement(i) {
   movieArr[i].likes++;
   document.getElementsByClassName("like-number")[i].innerHTML = movieArr[i].likes;
 }
-
-console.log(likeLink)
-
-
-// document.getElementById("sort-btn").addEventListener ("click", movieSort);
-
-// function movieSort() {
-// movieArr.sort(function(a, b){
-// return b.likes - a.likes;});
-// }
